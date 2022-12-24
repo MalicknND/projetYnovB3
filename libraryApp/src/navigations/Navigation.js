ff50e7ed74c3f0cac6c262ae027d2de9ddd00b5b
MalicknND <ndiayemalick062@gmail.com>
2022-12-24 02:13:22 +0100

    onboarding and loginscreen

--- libraryApp/src/navigations/Navigation.js
+++ libraryApp/src/navigations/Navigation.js
@@ -3,7 +3,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import OnBoardingScreen from '../screens/OnBoardingScreen';
-import SignUpScreen from '../screens/SignupScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import {View} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createNativeStackNavigator();
 
@@ -21,7 +23,29 @@ const Navigation = () => {
           component={LoginScreen}
           options={{headerShown: false}}
         />
-        <Stack.Screen name="Signup" component={SignUpScreen} />
+        <Stack.Screen
+          name="Signup"
+          component={SignUpScreen}
+          options={({navigation}) => ({
+            title: '',
+            headerStyle: {
+              backgroundColor: '#f9fafd',
+              // shadowColor: '#f9fafd',
+              elevation: 0,
+            },
+            headerLeft: () => {
+              <View>
+                <FontAwesome.Button
+                  name="long-arrow-left"
+                  size={25}
+                  backgroundColor="#f9fafd"
+                  color="#333"
+                  onPress={() => navigation.navigate('Login')}
+                />
+              </View>;
+            },
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- libraryApp/src/screens/SignUpScreen.js
+++ libraryApp/src/screens/SignUpScreen.js
@@ -1,21 +1,120 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import SocialButton from '../components/SocialButton';
 
-const SignUpScreen = () => {
+const SignUpScreen = ({navigation}) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
   return (
     <View style={styles.container}>
-      <AntDesign name="user" size={25} color="#6666" />
-      <Text>LoginScreen</Text>
+      <Text style={styles.text}>Créer un compte</Text>
+
+      <FormInput
+        labelValue={email}
+        onChangeText={userEmail => setEmail(userEmail)}
+        placeholder="Email"
+        iconType="user"
+        keyBoardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <FormInput
+        labelValue={password}
+        onChangeText={userPassword => setPassword(userPassword)}
+        placeholder="Mot de passe"
+        iconType="lock"
+        secureTextEntry={true}
+        autoCorrect={false}
+      />
+      <FormInput
+        labelValue={confirmPassword}
+        onChangeText={userPassword => setConfirmPassword(userPassword)}
+        placeholder="Confirmer Mot de passe"
+        iconType="lock"
+        secureTextEntry={true}
+        autoCorrect={false}
+      />
+      <FormButton
+        buttonTitle="S'inscrire"
+        onPress={() => alert('Se connecter')}
+      />
+      <View style={styles.textPrivate}>
+        <Text style={styles.color_textPrivate}>
+          By registering, you confirm that you accept our{' '}
+        </Text>
+        <TouchableOpacity onPress={() => alert('Terms Clicked!')}>
+          <Text style={[styles.color_textPrivate, {color: '#e88832'}]}>
+            Terms of service
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.color_textPrivate}> and </Text>
+        <Text style={[styles.color_textPrivate, {color: '#e88832'}]}>
+          Privacy Policy
+        </Text>
+      </View>
+
+      <SocialButton
+        buttonTitle="Se connecter avec facebook"
+        btnType="facebook"
+        color="#4867aa"
+        backgroundColor="#e6eaf4"
+        onPress={() => {}}
+      />
+      <SocialButton
+        buttonTitle="Se connecter avec google"
+        btnType="google"
+        color="#de4d41"
+        backgroundColor="#f5e7ea"
+        onPress={() => {}}
+      />
+      <TouchableOpacity
+        style={styles.forgotButton}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.navButtonText}>
+          Vous avez un compte?Connectez-vous.
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#f9fafd',
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    fontFamily: 'Kufam-SemiBoldItalic',
+    fontSize: 28,
+    marginBottom: 10,
+    color: '#051d5f',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#2e64e5',
+    fontFamily: 'Lato-Regular',
+  },
+  textPrivate: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 35,
+    justifyContent: 'center',
+  },
+  color_textPrivate: {
+    fontSize: 13,
+    fontWeight: '400',
+    fontFamily: 'Lato-Regular',
+    color: 'grey',
   },
 });
 
