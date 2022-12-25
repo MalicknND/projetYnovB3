07333639ba9442ba3d07220e3f6017469fa2f2ff
MalicknND <ndiayemalick062@gmail.com>
2022-12-25 18:18:53 +0100

    save work

--- libraryApp/src/components/FormButton.js
+++ libraryApp/src/components/FormButton.js
@@ -1,6 +1,5 @@
 import {StyleSheet, Text, TouchableOpacity, Dimensions} from 'react-native';
 import React from 'react';
-const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const FormButton = ({buttonTitle, ...rest}) => {
